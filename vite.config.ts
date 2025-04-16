bb01d616ca25df8ae3a307d7d13f32e1b8628adb
rust-master <muhammadzaryabrafique@gmail.com>
2025-04-16 15:04:35 +0500

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'rust-master.github.io',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
